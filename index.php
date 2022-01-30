228fbce06eae7b56aa2836a2163824c9973723ae
HaimyLe <H.le@lprs.fr>
2022-01-30 23:50:05 +0100

    delete affiche_vol.php merge code from affiche_vol.php into traitement.php use plugin datatable and select2 to display flights, pilots and airplanes

--- index.php
+++ index.php
@@ -8,9 +8,28 @@ $bdd = new PDO('mysql:host=localhost;dbname=hme_php_vol;charset=utf8',
 <head>
     <link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css"></link>
     <script type="text/javascript" src="http: //cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css"></link>
+    <script type="text/javascript" src="//cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready( function () {
+            $('#myTable').DataTable()
+        } );
+        $(document).ready(function() {
+            $('#ref_pilote').select2();
+        });
+        $(document).ready(function() {
+            $('#ref_avion').select2();
+        });
+    </script>
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <title>Booking Form HTML Template</title>
@@ -33,26 +52,26 @@ $bdd = new PDO('mysql:host=localhost;dbname=hme_php_vol;charset=utf8',
 
 </head>
 <body>
-<form method="post" action="ajout_traitement.php">
+<form method="post" action="traitement.php">
     <div id="booking" class="section">
         <div class="section-center">
             <div class="container">
                 <div class="row">
                     <div class="booking-form">
                         <form>
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <div class="form-group">
                                     <input class="form-control" type="date" name="date_depart" id="date" value="Date de depart">
                                     <span class="form-label">Date de depart</span>
                                 </div>
                             </div>
-                            <div class="col-md-3">
+                            <div class="col-md-4">
                                 <div class="form-group">
                                     <input class="form-control" type="time" name="heure_depart" id="depart" value="Heure de depart" required>
                                     <span class="form-label">Heure de depart</span>
                                 </div>
                             </div>
-                            <div class="col-md-3">
+                            <div class="col-md-4">
                                 <div class="form-group">
                                     <input class="form-control" type="time" name="heure_arrivee" id="arrival" value="Heure d'arrive" required>
                                     <span class="form-label">Heure d'arrivé</span>
@@ -94,14 +113,33 @@ $bdd = new PDO('mysql:host=localhost;dbname=hme_php_vol;charset=utf8',
                                     <button class="submit-btn">Enregistrer</button>
                                 </div>
                             </div>
-                            <div class="col-md-4">
-                                <div class="form-btn">
-                                    <button class="submit-btn" onclick="https://datatables.net/">Afficher vols</button>
-                                </div>
-                                <div class="form-btn">
-                                    <button class="submit-btn" onclick="">Afficher avions & pilotes</button>
-                                </div>
-                            </div>
+                            <table id="myTable">
+                                <thead>
+                                <tr>
+                                    <th>ID Vol</th>
+                                    <th>Date de Depart</th>
+                                    <th>Heure de depart</th>
+                                    <th>Heure d'arrivée</th>
+                                    <th>ID Pilote</th>
+                                    <th>ID Avion</th>
+                                </tr>
+                                <tbody>
+                                <?php
+                                $req = $bdd->query('SELECT * FROM vol');
+                                while($res=$req->fetch()){
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $res['id_vol'];?></td>
+                                        <td><?php echo $res['date_depart'];?></td>
+                                        <td><?php echo $res['heure_depart'];?></td>
+                                        <td><?php echo $res['heure_arrivee'];?></td>
+                                        <td><?php echo $res['ref_pilote'];?></td>
+                                        <td><?php echo $res['ref_avion'];?></td>
+                                    </tr>
+                                <?php } ?>
+                                </tbody>
+                                </thead>
+                            </table>
                         </form>
                     </div>
                 </div>
